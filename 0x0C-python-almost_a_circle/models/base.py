d02ffb2b39017d90ea23b09b8deee639bf58b741
Ahmedadel200066 <medo.adel200@gmail.com>
2023-10-28 17:04:57 +0200

    z

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
-
+"""
+module Base
+"""
 class Base:
+    """
+    Base class
+    """
 
-    __nb_objects = 0 
+    __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+        init - DG 3mr 7a7a remix
+        """
         if id is not None :
             self.id = id
         else :
