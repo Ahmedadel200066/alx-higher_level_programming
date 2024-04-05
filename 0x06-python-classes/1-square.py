a20b20c8fb6c2acacce6ce68a90f047f1ffc435e
Ahmedadel200066 <medo.adel200@gmail.com>
2024-04-05 15:39:48 +0200

    Refactor Square class constructor to include size parameter and fix indentation issue

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""define class name Square"""
+""" Defines a class named Square """
 
 
 class Square:
-    """This class represents a square."""
-    def __init__(self,size):
+    """ defines a function called __init__ """
+    def __init__(self, size):
         """ initializes __size of self with size """
-        self.__size= size
-        
+        self.__size = size
