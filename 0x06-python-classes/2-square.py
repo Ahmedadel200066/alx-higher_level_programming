79f5719ac79f6b3e66c3c625817e0569aee2d6ce
Ahmedadel200066 <medo.adel200@gmail.com>
2024-04-05 16:24:59 +0200

    Refactor Square class constructor to include input validation for size parameter and fix indentation issue

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,12 +5,12 @@
 class Square:
     """ defines a function called __init__ """
     def __init__(self, size = 0):
-        if type(size) != int :
+        if type(size) != int:
             """if statment type checker"""
             raise TypeError("size must be an integer")
-        elif size < 0 :
+        elif size < 0:
             """if size less than 0"""
-            raise TypeError("size must be >=0")
-        else
+            raise ValueError("size must be >=0")
+        else:
         """ initializes __size of self with size """
         self.__size = size
