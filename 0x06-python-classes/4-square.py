b12ff03610d18b945c0680928937ae4704353021
Ahmedadel200066 <medo.adel200@gmail.com>
2024-04-08 21:14:55 +0200

    Refactor Square class constructor to add size property and improve input validation

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -4,17 +4,19 @@
 
 class Square:
     """ defines a function named __init__ """
+
     def __init__(self, size=0):
         self.__size = size
         """ defines area function """
+
     def area(self):
         """ this function returns the current square area """
         return self.__size ** 2
     """ getter """
-    @property 
+    @property
     def size(self):
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         isinstance2 = isinstance(value, int)
