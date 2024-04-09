5f1e27f45092de98943c122f466cca7bf7ec47d7
Ahmedadel200066 <medo.adel200@gmail.com>
2024-04-09 05:22:57 +0200

    Refactor my_print method in Square class to use a nested loop for printing the square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -6,32 +6,33 @@ class Square:
     """ defines a function named __init__ """
 
     def __init__(self, size=0):
+        """ init """
         self.__size = size
-        """ defines area function """
 
-    def area(self):
-        """ this function returns the current square area """
-        return self.__size ** 2
-    """ getter """
     @property
     def size(self):
         return self.__size
 
     @size.setter
     def size(self, value):
-        isinstance2 = isinstance(value, int)
-        if isinstance2:
+
+        a = isinstance(value, int)
+        if a:
             self.__size = value
             if value < 0:
                 raise ValueError("size must be >= 0")
         else:
             raise TypeError("size must be an integer")
 
-    def my_print(self):
+    def area(self):
+        """ area method """
+        return (self.__size*self.__size)
 
-        if self._size == 0:
-            print()
+    def my_print(self):
+        """ print method """
+        if self.__size == 0:
+            print("")
         else:
-            for a in range(self.__size): 
-                 for e in range(self.__size): 
-                     print("".join("#" for z in range(self.__size)))
+            for a in range(self.__size):
+                for e in range(self.__size):
+                    print("".join("#" for z in range(self.__size)))
