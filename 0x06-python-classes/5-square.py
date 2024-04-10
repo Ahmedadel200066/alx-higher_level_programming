7d01d6720ca9400fd8b62e44be6abb78c4194236
Ahmedadel200066 <medo.adel200@gmail.com>
2024-04-10 03:31:25 +0200

    Refactor my_print method in Square class to use a nested loop for printing the square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,37 +1,40 @@
 #!/usr/bin/python3
-""" defines a class named Square """
+""" define a class Square """
 
 
 class Square:
-    """ defines a function named __init__ """
-
+    """ define __init__ function """
     def __init__(self, size=0):
-        """ init """
-        self.__size = size
+        """ initializes size of self with size """
+        self.size = size
 
     @property
     def size(self):
+        """ returns __size of self """
         return self.__size
 
     @size.setter
     def size(self, value):
-
-        a = isinstance(value, int)
-        if a:
-            self.__size = value
-            if value < 0:
-                raise ValueError("size must be >= 0")
-        else:
+        """ if statement """
+        if type(value) is not int:
+            """ raise error """
             raise TypeError("size must be an integer")
-
-    def area(self):
-        """ area method """
-        return (self.__size*self.__size)
-
-    def my_print(self):
-        """ print method """
-        if self.__size == 0:
-            print("")
+        elif value < 0:
+            """ raise error """
+            raise ValueError("size must be >= 0")
         else:
-            for a in range(self.__size):
-                print("".join("#" for z in range(self.__size)))
+            """ initialize """
+            self.__size = value
+    """ defines area function """
+    def area(self):
+        """ returns area """
+        return self.__size ** 2
+    """ defines my_print function """
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
