df5d2387264bb4b1516f7249841cc93d83cf90d6
Ahmedadel200066 <medo.adel200@gmail.com>
2024-04-10 03:20:41 +0200

    Refactor my_print method in Square class to remove unnecessary loop for printing the square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -33,5 +33,4 @@ class Square:
             print()
         else:
             for a in range(self.__size): 
-                for e in range(self.__size): 
-                    print("".join("#" for z in range(self.__size)))
+                print("".join("#" for z in range(self.__size)))
