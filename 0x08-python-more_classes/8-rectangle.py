2a4d760cd0954ef8a29620d28f4a044d440091ef
Ahmedadel200066 <medo.adel200@gmail.com>
2024-04-19 15:36:18 +0200

    Refactor Rectangle class bigger_or_equal method to handle equal areas correctly

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -74,10 +74,10 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() >= rect_2.area() :
+        if rect_1.area() >= rect_2.area():
             if rect_1.area() >= Rectangle.area():
                 return rect_1.area()
-        if rect_2.area() >= rect_1.area() :
+        if rect_2.area() >= rect_1.area():
             if rect_2.area() >= Rectangle.area():
                 return rect_2.area()
         if rect_1.area == rect_2.area == Rectangle.area:
