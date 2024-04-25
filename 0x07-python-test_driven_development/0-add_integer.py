65ed0ad435f3098fc767564e8d496b06ab30ad3b
Ahmedadel200066 <medo.adel200@gmail.com>
2024-04-25 02:30:12 +0200

    Update add_integer function documentation and remove unnecessary doctest

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-
+"""this is awesome"""
 def add_integer(a, b=98):
+    """this function like this sympol +"""
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
@@ -9,6 +10,3 @@ def add_integer(a, b=98):
     x = int(a)
     y = int(b)
     return x + y
-
-import doctest
-doctest.testfile("tests/0-add_integer.txt")
