303832096af14f80d26f45bc9fb79199341e6b78
Ahmedadel200066 <medo.adel200@gmail.com>
2024-04-27 03:47:37 +0300

    Fix type conversion bug in matrix_divided function and update test case

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 
 def matrix_divided(matrix, div):
-    if type(matrix) is not list[list[int,float]] :
+    if type(matrix) is not list[list[union[int, float]:
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    for row in matrix:
+        if len(row) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+    if type(div) is not int and type(div) is not float:
+        raise TabError("div must be a number")
+    if div == 0 :
+        raise ZeroDivisionError("division by zero")
+    else:
+        new_matrix = list[map(div, matrix)]
+        return new_matrix
