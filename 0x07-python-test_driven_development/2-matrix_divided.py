d40f1291a2c50ae1a4c2ae73e96b22a943ad8104
Ahmedadel200066 <medo.adel200@gmail.com>
2024-04-29 01:06:34 +0300

    Fix matrix_divided function to raise TypeError when rows have different sizes

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -12,7 +12,7 @@ def matrix_divided(matrix, div):
                         raise TypeError(
                             "matrix must be a matrix (list of lists) of \
                                 integers/floats")
-    if all(len(row) != len(matrix[0]) for row in matrix):
+    if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     if type(div) is not int and type(div) is not float:
         raise TabError("div must be a number")
