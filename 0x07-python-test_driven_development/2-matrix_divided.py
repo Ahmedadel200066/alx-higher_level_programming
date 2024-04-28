2db3914dffa7003d6d96b961a4d3d4a56bd9eb2d
Ahmedadel200066 <medo.adel200@gmail.com>
2024-04-28 18:58:53 +0300

    Refactor matrix_divided function to handle different types of matrices and improve code readability

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
 def matrix_divided(matrix, div):
-    if type(matrix) is not list[list[union[int, float]:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    for row in matrix:
-        if len(row) != len(matrix[0]):
-            raise TypeError("Each row of the matrix must have the same size")
+    if type(matrix) is not list:
+        for row in matrix:
+            if type(row) in matrix is not list:
+                for j in row :
+                    if type(j) is not int and type(j) is not float:   
+                        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     if type(div) is not int and type(div) is not float:
         raise TabError("div must be a number")
     if div == 0 :
         raise ZeroDivisionError("division by zero")
     else:
-        new_matrix = list[map(div, matrix)]
-        return new_matriX
+        return [[round(j / div, 2) for j in i] for i in matrix]
