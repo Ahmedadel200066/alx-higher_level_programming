c3a593f59ab6cbc8c34951ce6a2b4a3ad72d77f6
Ahmedadel200066 <medo.adel200@gmail.com>
2024-04-29 01:41:51 +0300

    Refactor matrix_divided function to improve type check and error handling

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,14 +4,12 @@
 
 def matrix_divided(matrix, div):
     """this function that divides all elements of a matrix"""
-    if type(matrix) is not list:
-        for row in matrix:
-            if type(row) is not list:
-                for j in row:
-                    if not isinstance(j, (int, float)):
-                        raise TypeError(
-                            "matrix must be a matrix \
-                                (list of lists) of integers/floats")
+    if (not isinstance(matrix, list) or matrix == [] or
+            not all(isinstance(row, list) for row in matrix) or
+            not all((isinstance(ele, int) or isinstance(ele, float))
+                    for ele in [num for row in matrix for num in row])):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     if type(div) is not int and type(div) is not float:
