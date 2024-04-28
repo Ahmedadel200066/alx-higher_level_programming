72f55ccef577a7fb2cee04ad5583518adbb9734b
Ahmedadel200066 <medo.adel200@gmail.com>
2024-04-29 01:26:58 +0300

    Refactor matrix_divided function to fix type check and improve error handling

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -8,7 +8,7 @@ def matrix_divided(matrix, div):
         for row in matrix:
             if type(row) is not list:
                 for j in row:
-                    if type(j) is not int and type(j) is not float:
+                    if not isinstance(j,(int,float)):
                         raise TypeError(
                             "matrix must be a matrix \
                                 (list of lists) of integers/floats")
