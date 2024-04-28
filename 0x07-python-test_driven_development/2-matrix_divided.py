62f55ff2f3de3467e9afa34214ab49f2de007c63
Ahmedadel200066 <medo.adel200@gmail.com>
2024-04-29 01:19:49 +0300

    Fix type check in matrix_divided function for invalid matrix input

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -6,9 +6,9 @@ def matrix_divided(matrix, div):
     """this function that divides all elements of a matrix"""
     if type(matrix) is not list:
         for row in matrix:
-            if type(row) in matrix is not list:
+            if type(row) is not list:
                 for j in row:
-                    if type(j) is not int or type(j) is not float:
+                    if type(j) is not int and type(j) is not float:
                         raise TypeError(
                             "matrix must be a matrix \
                                 (list of lists) of integers/floats")
