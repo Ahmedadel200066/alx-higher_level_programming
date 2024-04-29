2a077e8a021989b64550090b886efe974fef155c
Ahmedadel200066 <medo.adel200@gmail.com>
2024-04-29 03:39:30 +0300

    Add type hinting to say_my_name function in 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-
+"""this is say my name module"""
 def say_my_name(first_name, last_name=""):
+    """that function tells you your name"""
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
