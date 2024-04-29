596cb6f020f3df303d3ef3435022231b08df3ed8
Ahmedadel200066 <medo.adel200@gmail.com>
2024-04-29 03:39:58 +0300

    Fix spacing issue in say_my_name function

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 """this is say my name module"""
+
+
 def say_my_name(first_name, last_name=""):
     """that function tells you your name"""
     if type(first_name) is not str:
