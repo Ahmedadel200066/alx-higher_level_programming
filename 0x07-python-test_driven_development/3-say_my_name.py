6ec60705b7f6c6b18c6e09b52613578fce6d285f
Ahmedadel200066 <medo.adel200@gmail.com>
2024-04-29 03:37:56 +0300

    Refactor say_my_name function to fix spacing issue in print statement

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -2,9 +2,9 @@
 
 
 def say_my_name(first_name, last_name=""):
-    if type(first_name) is not str :
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    if type(last_name) is not str :
+    if type(last_name) is not str:
         raise TypeError("last_name must be a string")
     else:
-        print(format("My name is"),first_name,last_name)
+        print(format("My name is"), first_name, last_name)
