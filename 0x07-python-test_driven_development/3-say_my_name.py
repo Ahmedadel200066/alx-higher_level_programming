e730fb8494527c61add472e702f56b5975eba653
Ahmedadel200066 <medo.adel200@gmail.com>
2024-04-29 14:25:27 +0300

    Refactor print statement in say_my_name function to use format function

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -10,4 +10,4 @@ def say_my_name(first_name, last_name=""):
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
     else:
-        print(f"My name is {first_name} {last_name}")
+        print(format("My name is"), first_name, last_name)
