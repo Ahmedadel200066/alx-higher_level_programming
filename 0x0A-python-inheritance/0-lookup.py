8615214384caf749af2d65cc6e0f06d51ad524ab
Ahmedadel200066 <medo.adel200@gmail.com>
2024-05-06 22:06:49 +0300

    Refactor lookup function to improve code readability and remove unnecessary comments

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,15 +1,8 @@
 #!/bin/usr/python3
 
 """this method takes an obj as input and"""
+
+
 def lookup(obj):
-    """
-    This function takes an object as input and returns a list of all the available attributes and methods of that object.
-
-    Args:
-        obj: The object for which to retrieve the attributes and methods.
-
-    Returns:
-        A list of strings representing the attributes and methods of the object.
-
-    """
+    """that is returning the """
     return dir(obj)
