eb11282b87b6592f1d067149965dd4cdce5a0066
Ahmedadel200066 <medo.adel200@gmail.com>
2024-05-06 22:25:41 +0300

    Refactor MyList class to include an __init__ method

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,6 +4,9 @@ class MyList(list):
     """
     A custom list class that inherits from the built-in list class.
     """
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
 
     def print_sorted(self):
         """
