c29fb26301f72a5c630dc303de645cebaf9f4b8f
Ahmedadel200066 <medo.adel200@gmail.com>
2024-05-07 02:14:23 +0300

    Refactor is_kind_of_class function to remove unnecessary comment and clarify return statement

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -2,16 +2,7 @@
 
 """this is instance module"""
 def is_kind_of_class(obj, a_class):
-    """
-    Checks if an object is an instance of .
-
-    Args:
-        obj: The object to be checked.
-        a_class: The class to check against.
-
-    Returns:
-        True if the object is an instance of the class 
-    """
+    """ this is instance function """
     if isinstance(obj, a_class):
         return True
     else:
