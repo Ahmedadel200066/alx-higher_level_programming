19cdf055b6e5c3bee0206c83cecb950454f2d473
Ahmedadel200066 <medo.adel200@gmail.com>
2024-05-06 17:07:51 +0300

    s

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+""" this module for text indentation"""
+def text_indentation(text):
+    """
+    Splits the given text into paragraphs based on the occurrence of certain punctuation marks.
+    
+    Args:
+        text (str): The input text to be split into paragraphs.
+        
+    Raises:
+        TypeError: If the input text is not a string.
+        
+    Returns:
+        None
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    mark = 0
+    for a in text:
+        if mark == 0:
+            if a == ' ':
+                continue
+            else:
+                mark = 1
+        if mark == 1:
+            if a == '?' or a == '.' or a == ':':
+                print(a)
+                print()
+                mark = 0
+            else:
+                print(a, end="")
