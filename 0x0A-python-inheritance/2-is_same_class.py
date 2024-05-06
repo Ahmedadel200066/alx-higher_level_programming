aef71399bfc1971ac795ea9b5b336cf03894861c
Ahmedadel200066 <medo.adel200@gmail.com>
2024-05-07 01:54:58 +0300

    ads

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""this class is for list"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Checks if an object is an instance of a given class.
+
+    Args:
+        obj: The object to be checked.
+        a_class: The class to check against.
+
+    Returns:
+        True if the object is an instance of the class, False otherwise.
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
