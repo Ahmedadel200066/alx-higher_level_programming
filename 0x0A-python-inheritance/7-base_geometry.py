f3de53289ed9e4f42072d93839098651e54310c9
Ahmedadel200066 <medo.adel200@gmail.com>
2024-05-08 15:18:22 +0300

    Refactor BaseGeometry class for improved code reusability and readability

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -5,17 +5,7 @@
 
 
 class BaseGeometry():
-    """ 
-    BaseGeometry class 
-    represents the base class for geometrical shapes.
-
-    Methods:
-        - area(): Raises an exception indicating 
-                  that the method is not implemented.
-        - integer_validator(name, value): 
-        Validates if a given value is an integer and greater than 0
-
-    """
+    """represents the base class for geometrical shapes."""
 
     def area(self):
         """
@@ -25,17 +15,7 @@ class BaseGeometry():
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """
-        Validates if a given value is an integer and greater than 0
-
-        Args:
-            name (str): The name of the value being validated.
-            value (int): The value to be validated.
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than or equal to 0.
-        """
+        """given value is an integer and greater than 0"""
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         elif value <= 0:
