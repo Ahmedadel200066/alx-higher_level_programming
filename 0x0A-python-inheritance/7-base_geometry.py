76a963ab45576d9c6adcb8a49fe26c14890d42fa
Ahmedadel200066 <medo.adel200@gmail.com>
2024-05-08 15:03:41 +0300

    Refactor code to use BaseGeometry class for calculating area

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+"""this is geometry module"""
+
 
 class BaseGeometry():
     """ 
@@ -10,11 +12,26 @@ class BaseGeometry():
         - integer_validator(name, value): Validates if a given value is an integer and greater than 0.
 
     """
+
     def area(self):
+        """
+        This method raises an exception to indicate that the area calculation is not implemented.
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        if type(value) is not int :
-            raise TypeError("{} mast be an integer".format(name))
-        elif value <= 0 :
+        """
+        Validates if a given value is an integer and greater than 0.
+
+        Args:
+            name (str): The name of the value being validated.
+            value (int): The value to be validated.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than or equal to 0.
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
