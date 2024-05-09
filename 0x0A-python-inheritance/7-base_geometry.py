7a018b9b2927eb6ae72077ea180c14f96967c5f1
Ahmedadel200066 <medo.adel200@gmail.com>
2024-05-09 14:50:36 +0300

    Refactor error handling in BaseGeometry class

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -11,10 +11,10 @@ class BaseGeometry():
         area calculation is not implemented.
         """
         raise Exception("area() is not implemented")
-
+    
     def integer_validator(self, name, value):
         """given value is an integer and greater than 0"""
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
-        if value <= 0:
+        elif value <= 0:
             raise ValueError(f"{name} must be greater than 0")
