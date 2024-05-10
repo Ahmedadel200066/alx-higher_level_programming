c8368f6c7985f2fd8423390be2f6318189099e43
Ahmedadel200066 <medo.adel200@gmail.com>
2024-05-10 05:42:22 +0300

    Refactor Square class to inherit from Rectangle

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-Rectangle = __import__("9-rectangle").Rectangle
-
 """
 This module defines a Square class that represents a square shape.
 """
+Rectangle = __import__("9-rectangle").Rectangle
+
 
 class Square(Rectangle):
     """
