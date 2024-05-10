0c86782574874a6fae6c5cd8983ba30a7d650158
Ahmedadel200066 <medo.adel200@gmail.com>
2024-05-10 05:57:19 +0300

    Refactor Square class to use updated Square module and fix instantiation bug

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""
+This module defines a Square class that represents a square shape.
+"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """
+    Represents a square shape.
+
+    Attributes:
+        size (int): The size of the square.
+
+    Methods:
+        __init__(self, size): Initializes a new Square instance.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+        
+    def __str__(self):
+        """Returns a string representation of the rectangle.
+
+        Returns:
+            str: The string representation of the rectangle.
+        """
+        return "[Square] {}/{}".format(self.__width, self.__height)
