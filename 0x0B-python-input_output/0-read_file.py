4df7c5dc5ce16941baa791f5f488af838dd1703a
Ahmedadel200066 <medo.adel200@gmail.com>
2024-05-12 04:15:05 +0300

    chore: Refactor read_file function to improve code readability and maintainability

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+This module provides a function to read and print the contents of a file.
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads and prints the contents of a file.
+
+    Args:
+        filename (str): The name of the file to be read.
+
+    Returns:
+        None
+    """
+    with open(filename, "r") as file:
+        print(file.read())
