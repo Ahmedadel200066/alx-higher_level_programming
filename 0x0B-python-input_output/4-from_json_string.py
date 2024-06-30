c03ba8f7d1cead3e5864d11d80cbc5a44f2ce4a3
Ahmedadel200066 <medo.adel200@gmail.com>
2024-06-30 16:15:18 +0300

    sdf

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import json
+
+"""
+This module provides a function to convert a JSON string representation into a Python object.
+"""
+
+
+def from_json_string(my_str):
+    """
+    Converts a JSON string representation into a Python object.
+
+    Args:
+        my_str (str): The JSON string to be converted.
+
+    Returns:
+        The Python object representation of the JSON string.
+
+    """
+    return json.loads(my_str)
