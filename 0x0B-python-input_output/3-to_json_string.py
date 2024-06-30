763a56b7227de9825c061d5a3d7766ccb0f41c8b
Ahmedadel200066 <medo.adel200@gmail.com>
2024-06-30 16:01:50 +0300

    Refactor to_json_string function for improved code readability and documentation

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
 
+import json
+"""
+This module provides a function to convert an object to a JSON string representation.
+"""
+
 
 def to_json_string(my_obj):
-    
+    """
+    Convert the given object to a JSON string representation.
+
+    Args:
+        my_obj: The object to be converted.
+
+    Returns:
+        A JSON string representation of the object.
+    """
+    return json.dumps(my_obj)
