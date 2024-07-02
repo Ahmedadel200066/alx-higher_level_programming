e010cc2f635318310a8955f800c26807e7b6d065
Ahmedadel200066 <medo.adel200@gmail.com>
2024-07-02 21:05:08 +0300

    Refactor save_to_json_file function to remove unnecessary encoding parameter

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -14,5 +14,5 @@ def save_to_json_file(my_obj, filename=""):
     Returns:
         None
     """
-    with open(filename, "w", encoding="utf-8") as file:
+    with open(filename, "w") as file:
         json.dump(my_obj, file)
