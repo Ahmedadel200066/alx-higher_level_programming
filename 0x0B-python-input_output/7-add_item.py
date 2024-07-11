9c5e4afbbd9f7489c280c3350e1bc99500f0eec6
Ahmedadel200066 <medo.adel200@gmail.com>
2024-07-11 03:32:38 +0300

    Refactor code to use save_to_json_file function and fix typo in file name

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
-"""This module is about managing a JSON file.
-
-Functions:
-- save_to_json_file: Saves Python objects to a JSON file.
-- load_from_json_file: Loads Python objects from a JSON file.
+""" adds all arguments to a Python
+    list, and then save them to a file:
 """
-
-import os
 import sys
-save_to_json_file = __import__("5-save_to_json_file.py").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file.py").load_from_json_file
+import os
+save_to_json = __import__("5-save_to_json_file").save_to_json_file
+load_from_json = __import__("6-load_from_json_file").load_from_json_file
 
-if (os.path.exists("add_item.json")):
-    new_save = load_from_json_file("add_item.json")
-else:
-    new_save = []
-    new_save.extend(sys.argv[1:])
-    save_to_json_file(new_save, "add_item.json")
+
+def add_item(args, filename):
+    if (os.path.exists(filename)):
+        content = load_from_json(filename)
+    else:
+        content = []
+    content.extend(args)
+    save_to_json(content, filename)
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    filename = "add_item.json"
+    add_item(args, filename)
