a33216bae6ac6561e3249a96dfae7b90372bb02f
Ahmedadel200066 <medo.adel200@gmail.com>
2024-07-11 03:39:30 +0300

    Refactor code to use save_to_json_file function and fix typo in file name

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,10 +8,11 @@ save_to_json = __import__("5-save_to_json_file").save_to_json_file
 load_from_json = __import__("6-load_from_json_file").load_from_json_file
 if __name__ == "__main__":
 
-if (os.path.exists("add_item.json")):
+if(os.path.exists("add_item.json")):
    New_save = load_from_json("add_item.json")
 else:
    New_save = []
-   New_save.extend(sys.argv[1:])
+   args = sys.argv[1:]
+   New_save.extend(args)
    save_to_json(New_save, "add_item.json")
    
