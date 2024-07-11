5b8576ee028d5ae14629451ab82327d73a7ae128
Ahmedadel200066 <medo.adel200@gmail.com>
2024-07-11 03:29:08 +0300

    Refactor code to use save_to_json_file function for improved code readability and documentation

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -15,8 +15,7 @@ Functions:
 import os
 import sys
 save_to_json_file = __import__("5-save_to_json_file.py").save_to_json_file
-load_from_json_file = __import__(
-    "6-load_from_json_file.py").load_from_json_file
+load_from_json_file = __import__("6-load_from_json_file.py").load_from_json_file
 
 if (os.path.exists("add_item.json")):
     new_save = load_from_json_file("add_item.json")
