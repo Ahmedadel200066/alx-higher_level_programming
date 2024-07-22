e39df6d25e68d91a73cc809ad563be04fc0ca486
Ahmedadel200066 <medo.adel200@gmail.com>
2024-07-22 12:14:37 +0300

    Refactor Square class __str__ method for proper string representation

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,6 +17,14 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
+    def __str__():
+        f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
 
-def __str__():
-    f"[Square] ({id}) {self.x}/{self.y} - {self.size}"
+    @property
+    def size(self):
+        return self.size
+
+    @size.setter
+    def size(self, size):
+        self.width = size
+        self.height = size
